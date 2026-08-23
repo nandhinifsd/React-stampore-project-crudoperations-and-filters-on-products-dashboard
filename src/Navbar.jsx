5f4510d738fd3fdb43e5549b6f4b386c2d5b0124
Nandhini <coolnaandhi@gmail.com>
2026-08-23 18:12:42 +0530

    Highlighted the active navlink

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link,useLocation } from 'react-router-dom';
 import { useState } from 'react';
 
 
 const Navbar = () => {
     const[isOpen,setIsOpen]=useState(false);
+    const location=useLocation();
+  function getLinkClass(path) 
+  {
+  let isActive=false;
+
+  if ((path === "/") &&(location.pathname===path)) {
+    isActive = true;
+  } else if((path !== "/") && (location.pathname.startsWith(path)) ) {
+    isActive = true;
+  }
+
+  if (isActive) {
+    return "px-2 py-2 my-2 text-blue-700";
+  } else {
+    return "px-2 py-2 my-2 text-blue-900";
+  }
+};
   return (
     <div>
         <nav className='navbar-container flex flex-row  w-screen h-auto py-2 px-4 bg-gradient-to-r from-cyan-100 to-teal-100 to-yellow-100 justify-between'>
@@ -13,9 +30,9 @@ const Navbar = () => {
             <h1 className='title-txt hidden lg:flex  lg:text-4xl font-serif bg-gradient-to-r from-blue-950 to-indigo-600 bg-clip-text text-transparent py-2 px-1 my-2 mx-3 font-black'>STAMPORA DASHBOARD</h1>
                 </div>
                 <div className='link-container hidden md:flex flex-column w-sm ps-4 pe-1 py-1 gap-2 text-xl font-bold font-serif text-blue-900'>
-                        <Link className='px-2 py-2 my-2' to="/">Home</Link>
-                        <Link className='px-2 py-2 my-2' to="/products">Products</Link>
-                        <Link className='px-2 py-2 my-2'to="/customers">Customers</Link>
+                        <Link className={getLinkClass("/")} to="/">Home</Link>
+                        <Link className={getLinkClass("/products")} to="/products">Products</Link>
+                        <Link className={getLinkClass("/customers")} to="/customers">Customers</Link>
                         
                 </div>
                 <button className='md:hidden text-2xl bg-transparent p-2' onClick={()=>setIsOpen(!isOpen)} >
